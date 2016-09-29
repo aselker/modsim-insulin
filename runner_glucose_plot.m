a570d528a41227288b0805f5e7194838872fb300
yjiang <pauljyc0929@gmail.com>
2016-09-29 14:43:32 -0400

    yeah

--- runner_glucose_plot.m
+++ runner_glucose_plot.m
@@ -1,4 +1,4 @@
-[T, insulin, glucose] = sim_glucose(5*10^-5, 7.2e-6, 0.067, 0.008, 2500, 4000, 500000, 0.1, 130);
+[T, insulin, glucose] = sim_glucose(18*10^-5, 21e-6, 0.010, 0.008, 2500, 4000, 500000, 0.1, 130);
 
 
 %plot(T, insulin, 'r.');
