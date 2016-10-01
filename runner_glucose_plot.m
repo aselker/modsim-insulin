8c45569bf45e7b5585c1ee86204fa73c1391fe23
yjiang <pauljyc0929@gmail.com>
2016-10-01 19:30:12 -0400

    I'm just lazy

--- runner_glucose_plot.m
+++ runner_glucose_plot.m
@@ -1,8 +1,9 @@
 %S0, B0, ka, ke, food_glucose, blood_glucose, glucose_consumption_rate_constant, glucose_absorbtion_rate_constant, digestion_rate_constant
 %[T, insulin, glucose] = sim_glucose(18*10^-5, 21e-6, 0.010, 0.008, 2500, 4000, 500000, 0.1, 130);
 %Old, will be modified to fit new data
-[T, insulin, glucose] = sim_glucose(18*10^-5, 21e-6, 0.010, 0.008, 14000, 4000, 500000, 0.016, 80);
+%[T, insulin, glucose] = sim_glucose(18*10^-5, 21e-6, 0.010, 0.008, 14000, 4000, 500000, 0.016, 80);
+[T, insulin, glucose] = sim_glucose(6*10^-5, 5e-6, 0.09, 0.008, 2500, 4000, 500000, 0.1, 130);
 
-%plot(T, insulin, 'r.');
+plot(T, insulin, 'r.');
 %or
-plot(T, glucose, 'g.');
+%plot(T, glucose, 'g.');
