eb0f64ede6d192e2f2f23372b7c6ad9a116bba85
aselker <aselker@openmailbox.org>
2016-10-01 19:06:37 -0400

    Made slow eating work, created peak/trough tracker script.

--- flow_glucose_consumption.m
+++ flow_glucose_consumption.m
@@ -1,9 +1,9 @@
 function out = flow_glucose_consumption (food, glucose_consumption_rate_constant)
 
-    %if (food >= glucose_consumption_rate_constant)
-    %    out = glucose_consumption_rate_constant;
-    %else
+    if (food >= glucose_consumption_rate_constant)
+        out = glucose_consumption_rate_constant;
+    else
         out = food;
-    %end
+    end
     
 end
---
+++ runner_glucose_peak_trough.m
@@ -0,0 +1,10 @@
+S0 = 1.8e-4;
+B0 = 2.1e-5;
+ka = 0.010;
+ke = 0.008;
+food_glucose = 14000;
+blood_glucose = 4000;
+glucose_consumption_rate_constant = 500000;
+glucose_absorbtion_rate_constant = 0.016;
+digestion_rate_constant = 80;
+
